2e4c1d47ff7e9ffc71482b328d1d315a1e845513
Riku Kilpimaa <RikuKilpimaa@hotmail.fi>
2025-03-21 11:57:28 +0200

    Remove visma lib

--- visma-pay-php-lib/lib/visma_pay_loader.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once dirname(__FILE__) . '/VismaPay/VismaPay.php';
-require_once dirname(__FILE__) . '/VismaPay/VismaPayConnector.php';
-require_once dirname(__FILE__) . '/VismaPay/VismaPayCurl.php';
-require_once dirname(__FILE__) . '/VismaPay/VismaPayException.php';
